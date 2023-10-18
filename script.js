5ff8a77ff1cf4c6a78446bc40276ede6b8e2052b
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-17 21:45:24 -0300

    working with chart

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-Plotly.plot( document.querySelector('#chart'), 
-  [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16] 
-  }], 
-  { margin: { t: 0 }})
+let montante = Number(Montante.value)
+let taxa = Number(Taxa.value) / 100
+// let meses = Number(Meses.value)
+let parcela = Number(Parcela.value)
+
+let divida = [montante]
+let juros = [0]
+let I = []
+for(let i = 0;  divida[0] > 0  ; i ++) {
+  juros.unshift(divida[0] * taxa)
+  divida.unshift(divida[0] + juros[0] - parcela)
+  I.push(i)
+}
+divida.reverse()
+juros.reverse()
+
+
+Plotly.plot( 
+  document.querySelector('#chart'), 
+  [{ x: I, y: divida }], 
+  { margin: { t: 0 }}
+)

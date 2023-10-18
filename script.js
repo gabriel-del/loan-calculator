bdfed8352403fa394d3598f884b0f1e3932c44aa
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-17 22:01:11 -0300

    2 charts working

--- script.js
+++ script.js
@@ -15,8 +15,48 @@ divida.reverse()
 juros.reverse()
 
 
-Plotly.plot( 
-  document.querySelector('#chart'), 
-  [{ x: I, y: divida }], 
-  { margin: { t: 0 }}
-)
+// Plotly.plot( 
+//   document.querySelector('#chart'), 
+//   [{ x: I, y: divida }], 
+//   { margin: { t: 0 }}
+// )
+
+
+let trace1 = {
+  x: I,
+  y: divida,
+  name: 'Dívida',
+  type: 'scatter'
+};
+
+let trace2 = {
+  x: I,
+  y: juros,
+  name: 'Juros',
+  yaxis: 'y2',
+  type: 'scatter'
+};
+
+let data = [trace1, trace2];
+
+let layout = {
+  title: 'multiple y-axes example',
+  width: 800,
+  xaxis: {domain: [0.3, 0.7]},
+  yaxis: {
+    title: 'yaxis title',
+    titlefont: {color: '#1f77b4'},
+    tickfont: {color: '#1f77b4'}
+  },
+  yaxis2: {
+    title: 'yaxis2 title',
+    titlefont: {color: '#ff7f0e'},
+    tickfont: {color: '#ff7f0e'},
+    anchor: 'free',
+    overlaying: 'y',
+    side: 'left',
+    position: 0.15
+  }
+};
+
+Plotly.newPlot(document.querySelector('#chart'), data, layout);

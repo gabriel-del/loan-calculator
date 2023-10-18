ac9245d5709388840be6356ef792a2409895f266
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-17 22:50:11 -0300

    charts being updates

--- script.js
+++ script.js
@@ -1,52 +1,66 @@
-let montante = Number(Montante.value)
-let taxa = Number(Taxa.value) / 100
-// let meses = Number(Meses.value)
-let parcela = Number(Parcela.value)
+let montante, taxa, parcela, divida, juros, I
 
-let divida = [montante]
-let juros = [0]
-let I = []
-for(let i = 0;  divida[0] > 0  ; i ++) {
-  juros.unshift(divida[0] * taxa)
-  divida.unshift(divida[0] + juros[0] - parcela)
-  I.push(i)
+function load() {
+  Plotly.purge(document.querySelector('#chart'))
+  // Plotly.deleteTraces(document.querySelector('#chart'), 0)
+  getValues()
+  plotCharts()
 }
-divida.reverse()
-juros.reverse()
+load()
 
+document.addEventListener("DOMContentLoaded", () => {
+  Montante.addEventListener('input', load)
+  Taxa.addEventListener('input', load)
+  Parcela.addEventListener('input', load)
+})
 
-// Plotly.plot( 
-//   document.querySelector('#chart'), 
-//   [{ x: I, y: divida }], 
-//   { margin: { t: 0 }}
-// )
-
-
-Plotly.newPlot(document.querySelector('#chart'), 
-[{
-  x: I,
-  y: divida,
-  name: 'Dívida'
-}, {
-  x: I,
-  y: juros,
-  name: 'Juros',
-  yaxis: 'y2'
-}], 
-{
-  title: 'Variação da dívida',
-  yaxis: {
-    title: 'Dívida',
-    titlefont: {color: '#1f77b4'},
-    tickfont: {color: '#1f77b4'}
-  },
-  yaxis2: {
-    title: 'Juros',
-    titlefont: {color: '#ff7f0e'},
-    tickfont: {color: '#ff7f0e'},
-    anchor: 'free',
-    overlaying: 'y',
-    side: 'right',
-    position: 0.15
+function getValues() {
+  montante = Number(Montante.value)
+  taxa = Number(Taxa.value) / 100
+  // meses = Number(Meses.value)
+  parcela = Number(Parcela.value)
+  
+  divida = [montante]
+  juros = [0]
+  I = []
+  for(let i = 0;  divida[0] > 0  ; i ++) {
+    juros.unshift(divida[0] * taxa)
+    divida.unshift(divida[0] + juros[0] - parcela)
+    I.push(i)
   }
-});
+  divida.reverse()
+  juros.reverse()
+}
+
+
+function plotCharts() {
+  Plotly.newPlot(document.querySelector('#chart'), 
+  [{
+    x: I,
+    y: divida,
+    name: 'Dívida'
+  }, {
+    x: I,
+    y: juros,
+    name: 'Juros',
+    yaxis: 'y2'
+  }], 
+  {
+    title: 'Variação da dívida e do juros',
+    yaxis: {
+      title: 'Dívida',
+      titlefont: {color: '#1f77b4'},
+      tickfont: {color: '#1f77b4'}
+    },
+    yaxis2: {
+      title: 'Juros',
+      titlefont: {color: '#ff7f0e'},
+      tickfont: {color: '#ff7f0e'},
+      anchor: 'x',
+      overlaying: 'y',
+      side: 'right',
+      position: 0.45
+    }
+  })
+}
+
